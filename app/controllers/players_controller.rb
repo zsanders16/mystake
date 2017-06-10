d8fa6d828dedd558c8c50d17e059991fcf28aa3f
Zachary Sanders <zsanders@icloud.com>
2017-06-10 15:06:38 -0600

    player cards added to player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,25 @@
 class PlayersController < ApplicationController
 
     def create
+    
         @game = Game.find(params[:game_id])
-        @player = @game.players.new
-
+        @player = @game.players.new(player_params)
         @player.save
+
+        5.times do
+            @player.player_cards.new(answer_card_id: AnswerCard.all.sample.id)
+        end
+        binding.pry
+        
         redirect_to game_path(params[:game_id])
+
     end
     
+
+    private 
+
+        def player_params
+            params.require(:player).permit(:name)
+        end
+        
 end
