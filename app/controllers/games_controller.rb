89742a9503a861526b418c67fb2ce604daebc325
Zachary Sanders <zsanders@icloud.com>
2017-06-10 16:11:19 -0600

    bug fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
   def index
     @game = Game.create
     5.times do
-      #@game.game_cards.create(sentence_card_id: SentenceCards.all.sample.id)
+      @game.game_cards.create(sentence_card_id: SentenceCard.all.sample.id)
     end
   end
 
