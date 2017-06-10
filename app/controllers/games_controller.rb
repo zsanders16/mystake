0c9df80e6416f1c18a86697c4bad69cb9153be72
Zachary Sanders <zsanders@icloud.com>
2017-06-10 10:58:48 -0600

    add routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,9 @@
 class GamesController < ApplicationController
   def Index
   end
+
+  def New
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  get 'sentence_cards/index'
-
-  get 'sentence_cards/new'
-
-  get 'sentence_cards/edit'
-
-  get 'answer_cards/index'
-
-  get 'answer_cards/new'
-
-  get 'answer_cards/edit'
 
   root 'games#Index'
 
   devise_for :users
+  resources :game
+  resources :player
+  resources :answer_card
+  resources :sentence_card
+  resources :game_card
+  resources :player_card
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
